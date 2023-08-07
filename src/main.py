68faa67ab876af868c0ccd73db11537c101c9af3
Mitchell Merry <mitchellmerry145@gmail.com>
2023-08-07 15:54:28 +1000

    Get tkinter to work with rendering .fits files (#1)
    * Get .fits file reading to work
    
    * Remove PyQT
    
    ---------
    
    Co-authored-by: Mitchell Merry <mmerry@atlassian.com>

--- src/main.py
+++ src/main.py
@@ -1,16 +1,37 @@
 import tkinter as tk
+from astropy.io import fits
+from astropy.visualization import LogStretch
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
-window = tk.Tk()
-window.title("Hello World")
+# Read the .fits file
+image_file = './data/sample/Optical_r.fits'
+hdu_list = fits.open(image_file)
+image_data = hdu_list[0].data
 
+# Apply logarithmic scaling to the image data
+log_stretch = LogStretch()
+scaled_data = log_stretch(image_data)
 
-def handle_button_press(event):
-    window.destroy()
+# Create a tkinter window
+root = tk.Tk()
+root.title("FITS Image Viewer")
 
+# Create a matplotlib figure
+fig = Figure(figsize=(5, 5), dpi=100)
+ax = fig.add_subplot(111)
 
-button = tk.Button(text="My simple app.")
-button.bind("<Button-1>", handle_button_press)
-button.pack()
+# Render the scaled image data onto the figure
+cax = ax.imshow(scaled_data, cmap='gray', origin='lower')
 
-# Start the event loop.
-window.mainloop()
+# Add a colorbar
+fig.colorbar(cax)
+
+# Embed the matplotlib figure in the tkinter window
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+# Run the tkinter main loop
+root.mainloop()
