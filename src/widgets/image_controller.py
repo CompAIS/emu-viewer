c1bf50410e3f97742d576750afea8c438a0c3dde
Mitchell Merry <mitchellmerry145@gmaill.com>
2023-08-11 14:12:43 +1000

    Remove the padding, axes, and colourbar

--- src/widgets/image_controller.py
+++ src/widgets/image_controller.py
@@ -53,13 +53,13 @@ class ImageFrame(ttk.Frame):
 
         # Create a matplotlib figure
         fig = Figure(figsize=(5, 5), dpi=100)
-        ax = fig.add_subplot(111)
+        ax = fig.add_subplot()
+        fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        ax.margins(0, 0)
+        ax.axis("off")
 
         # Render the scaled image data onto the figure
         cax = ax.imshow(scaled_data, cmap="gray", origin="lower")
-
-        # Add a colourbar
-        fig.colorbar(cax)
 
         # Embed the matplotlib figure in the tkinter window
         canvas = FigureCanvasTkAgg(fig, master=self)
