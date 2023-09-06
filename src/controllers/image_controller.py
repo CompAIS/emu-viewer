fce792330b8a2c6af793c371a161a96a54a66c75
Mitchell Merry <mitchellmerry145@gmail.com>
2023-09-06 14:20:33 +1000

    slight formatting change

--- src/controllers/image_controller.py
+++ src/controllers/image_controller.py
@@ -8,19 +8,10 @@ from src.widgets import image_widget as iw
 # Create Image Controller Frame
 class ImageController(tb.Frame):
     def __init__(self, parent, root):
-        tb.Frame.__init__(
-            self,
-            parent,
-            bootstyle="dark",
-        )
-
-        self.grid(
-            column=1,
-            row=0,
-            sticky=tk.NSEW,
-        )
+        tb.Frame.__init__(self, parent, bootstyle="dark")
 
         self.root = root
+        self.grid(column=1, row=0, sticky=tk.NSEW)
         self.rowconfigure(0, weight=1, uniform="c")
         self.columnconfigure(0, weight=1, uniform="r")
 
