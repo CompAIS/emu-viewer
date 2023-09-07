5857afcb24e3933b82f4217f10f80a0f1ccdce2b
RyanW <ryanwartmann@hotmail.com>
2023-09-08 00:11:19 +1000

    Basic wireframing added

--- src/controllers/widget_controller.py
+++ src/controllers/widget_controller.py
@@ -1,20 +1,18 @@
-from src.widgets import histogram_widget as histogram_widget
-from src.widgets import image_table_widget as image_table_widget
+from src.widgets import image_table_widget, renderer_widget
 
 
 class WidgetController:
-    open_windows = []
-
     def __init__(self, root):
         self.root = root
+        self.open_windows = []
 
-        root.menu_controller.open_histogram_eh.add(self.open_histogram_widget)
+        root.menu_controller.open_renderer_eh.add(self.open_renderer_widget)
 
         root.menu_controller.open_image_table_eh.add(self.open_image_table_widget)
 
-    def open_histogram_widget(self):
-        new_histogram = histogram_widget.HistogramWidget(self.root)
-        self.open_windows.append(new_histogram)
+    def open_renderer_widget(self):
+        new_renderer = renderer_widget.RendererWidget(self.root)
+        self.open_windows.append(new_renderer)
 
     def open_image_table_widget(self):
         new_image_table = image_table_widget.ImageTableWidget(self.root)
--- src/widgets/image_table_widget.py
+++ src/widgets/image_table_widget.py
@@ -1,11 +1,23 @@
 import tkinter as tk
 
+import ttkbootstrap as tb
+
 
 class ImageTableWidget(tk.Toplevel):
     def __init__(self, root):
         tk.Toplevel.__init__(self, root)
         self.title("Image Table")
-        self.geometry("500x500")
+        self.geometry("500x250")
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
+
+        self.image_table()
+
+    def image_table(self):
+        table = tb.Frame(self, bootstyle="light")
+        table.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
+        table.grid_propagate(0)
+
+        label = tb.Label(table, text="Image Table", bootstyle="success")
+        label.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
