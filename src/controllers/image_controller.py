1775004c63a7d805f15ec384240c958c20ba9935
Mitchell Merry <mitchellmerry145@gmail.com>
2023-09-25 16:04:20 +1000

    NONE Expose selected image from image controller (#26)
    Co-authored-by: Mitchell Merry <mitchellmerry145@gmaill.com>

--- src/controllers/image_controller.py
+++ src/controllers/image_controller.py
@@ -1,9 +1,9 @@
-import os  # Used to obtain file name from path
 import tkinter as tk
 
 import ttkbootstrap as tb
 
 from src.widgets import image_widget as iw
+from src.widgets.image_standalone_toplevel import StandaloneImage
 
 
 # Create Image Controller Frame
@@ -16,6 +16,8 @@ class ImageController(tb.Frame):
         self.rowconfigure(0, weight=1, uniform="c")
         self.columnconfigure(0, weight=1, uniform="r")
 
+        self.selected_image = -1
+
         # Add open_image as an event listener to open file
         root.menu_controller.open_file_eh.add(self.open_image)
         root.menu_controller.append_image_eh.add(self.append_image)
@@ -23,26 +25,50 @@ class ImageController(tb.Frame):
         self.open_windows = []
 
     def open_image(self, file_path):
-        self.main_image = iw.ImageFrame(self, self.root, file_path)
         self.close_windows()
+        self.main_image = iw.ImageFrame(self, self.root, file_path)
+        self.set_selected_image(0)
 
     def append_image(self, file_path):
         if self.main_image is None:
-            self.open_image(self, file_path)
+            self.open_image(file_path)
             return
 
-        file_name = os.path.basename(file_path)
+        image_id = len(self.open_windows) + 1
+        new_window = StandaloneImage(self, file_path, image_id)
+        self.set_selected_image(image_id)
 
-        new_window = tk.Toplevel(self.root)
-        new_window.title(file_name)
-        new_window.geometry("800x600")
-        new_window.rowconfigure(0, weight=1)
-        new_window.columnconfigure(0, weight=1)
-
-        new_image = iw.ImageFrame(new_window, self.root, file_path)
         self.open_windows.append(new_window)
 
+    def get_selected_image(self):
+        if self.selected_image == -1:
+            return None  # No image loaded so nothing to select
+
+        if self.selected_image == 0:
+            return self.main_image
+
+        return self.open_windows[self.selected_image - 1].image_frame
+
+    def get_images(self):
+        images = [self.main_image]
+        for w in self.open_windows:
+            images.append(w.image_frame)
+
+        return images
+
+    def set_selected_image(self, image):
+        self.selected_image = image
+
     def close_windows(self):
+        if self.main_image is not None:
+            self.main_image.close()
+
         for window in self.open_windows:
+            window.image_frame.close()
             window.destroy()
+
         self.open_windows = []
+        self.set_selected_image(-1)
+
+    def handle_focus(self, event):
+        self.set_selected_image(0)
