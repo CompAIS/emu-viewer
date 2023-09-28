ab8c6cf01717470a35cbd33d6c063989abd10827
Ayman Nachabe <ayman1.nachabe@gmail.com>
2023-09-28 21:56:36 +1000

    Added name and num functionality

--- src/widgets/image_table_widget.py
+++ src/widgets/image_table_widget.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 
@@ -6,34 +7,61 @@ class ImageTableWidget(tk.Toplevel):
     def __init__(self, root):
         tk.Toplevel.__init__(self, root)
         self.title("Image Table")
-        self.geometry("500x250")
+        self.geometry("500x500")
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        # Create a frame for the table
-        table_frame = ttk.Frame(self)
-        table_frame.grid(row=0, column=0, sticky=tk.NSEW)
+        self.image_table()  # Create the image table widget
 
-        # Create a table (Treeview) with two columns: "Num" and "Image Name"
-        self.table = ttk.Treeview(
-            table_frame, columns=("Num", "Image Name"), show="headings"
+        self.image_num = 0  # Initialize the image number
+
+    def image_table(self):
+        table = tk.Frame(self)
+        table.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
+        table.grid_propagate(0)
+
+        label = tk.Label(table, text="Image Table")
+        label.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
+
+        self.tree = ttk.Treeview(
+            table, columns=("Num", "Image", "Coords Matching", "Render Matching")
         )
-        self.table.heading("Num", text="Num")
-        self.table.heading("Image Name", text="Image Name")
+        self.tree.heading("#1", text="Num")
+        self.tree.heading("#2", text="Image")
+        self.tree.heading("#3", text="Coords Matching")
+        self.tree.heading("#4", text="Render Matching")
+        self.tree.grid(column=0, row=1, sticky="nsew")
 
-        # Pack the table and set its column widths
-        self.table.pack(fill=tk.BOTH, expand=True)
-        self.table.column("Num", width=50)
-        self.table.column("Image Name", width=200)
+    def add_image(self, image_path):
+        self.image_num += 1
+        # Extract the file name from the image_path
+        image_name = os.path.basename(image_path)
+        self.tree.insert("", "end", values=(self.image_num, image_name, "", ""))
 
-        # Initialize the image counter
-        self.image_counter = 0
+    def remove_image(self):
+        selected_item = self.tree.selection()
+        if selected_item:
+            self.tree.delete(selected_item)
+            self.update_image_numbers()
 
-    def add_image(self, image_name):
-        # Increment the image counter
-        self.image_counter += 1
-        num = self.image_counter
+    def update_image_numbers(self):
+        for i, item in enumerate(self.tree.get_children()):
+            self.tree.item(item, values=(i + 1,) + self.tree.item(item, "values")[1:])
 
-        # Add the image information to the table
-        self.table.insert("", "end", values=(num, image_name))
+
+if __name__ == "__main__":
+    root = tk.Tk()
+
+    # Example: Create the ImageTableWidget
+    image_table_widget = ImageTableWidget(root)
+
+    # Example: Add images
+    # image_table_widget.add_image("C:/Users/ayman/Downloads/Optical_r.fits")
+    # image_table_widget.add_image("path/to/Image2.png")
+    # image_table_widget.add_image("path/to/Image3.png")
+
+    # Example: Remove an image
+    # image_table_widget.remove_image()
+
+    root.mainloop()
