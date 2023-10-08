c8c9fcb0d7a018dfe01ec5c74ceb27cef9824678
Mitchell Merry <mitchellmerry145@gmail.com>
2023-10-08 16:45:03 +1100

    Data source dropdown

--- src/widgets/contour_widget.py
+++ src/widgets/contour_widget.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+from functools import partial
 
 import ttkbootstrap as tb
 
 from src.widgets.base_widget import BaseWidget
+
+NOTHING_OPEN = "No image open"
 
 
 class ContourWidget(BaseWidget):
@@ -11,6 +14,8 @@ class ContourWidget(BaseWidget):
 
     def __init__(self, root):
         super().__init__(root)
+
+        self.selected_image = None
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
@@ -21,34 +26,51 @@ class ContourWidget(BaseWidget):
         self.frame.grid_columnconfigure(0, weight=1)
         self.frame.grid_columnconfigure(1, weight=2)
 
+        # Data Source
+        data_source_label = tb.Label(
+            self.frame, text="Data Source", bootstyle="inverse-light"
+        )
+        data_source_label.grid(column=0, row=0, padx=10, pady=10, sticky=tk.W)
+
+        self.data_source_dropdown = tb.Menubutton(
+            self.frame, text=NOTHING_OPEN, bootstyle="dark"
+        )
+        self.data_source_dropdown.grid(column=1, row=0, padx=10, pady=10)
+
+        self.root.image_controller.update_image_list_eh.add(self.update_dropdown)
+        self.update_dropdown(
+            self.root.image_controller.get_selected_image(),
+            self.root.image_controller.get_images(),
+        )
+
         parameters_label = tb.Label(
             self.frame,
             text="Parameters",
             bootstyle="inverse-light",
             font=("Helvetica bold", 10),
         )
-        parameters_label.grid(column=0, row=0, padx=10, pady=10, sticky=tk.W)
+        parameters_label.grid(column=0, row=1, padx=10, pady=10, sticky=tk.W)
 
         # Parameters
         mean_label = tb.Label(self.frame, text="Mean", bootstyle="inverse-light")
-        mean_label.grid(column=0, row=1, padx=10, pady=10, sticky=tk.W)
+        mean_label.grid(column=0, row=2, padx=10, pady=10, sticky=tk.W)
 
         mean_entry = tb.Entry(self.frame)
-        mean_entry.grid(column=1, row=1, padx=10, pady=10)
+        mean_entry.grid(column=1, row=2, padx=10, pady=10)
 
         sigma_label = tb.Label(self.frame, text="Sigma", bootstyle="inverse-light")
-        sigma_label.grid(column=0, row=2, padx=10, pady=10, sticky=tk.W)
+        sigma_label.grid(column=0, row=3, padx=10, pady=10, sticky=tk.W)
 
         sigma_entry = tb.Entry(self.frame)
-        sigma_entry.grid(column=1, row=2, padx=10, pady=10)
+        sigma_entry.grid(column=1, row=3, padx=10, pady=10)
 
         sigmas_label = tb.Label(
             self.frame, text="Sigma List", bootstyle="inverse-light"
         )
-        sigmas_label.grid(column=0, row=3, padx=10, pady=10, sticky=tk.W)
+        sigmas_label.grid(column=0, row=4, padx=10, pady=10, sticky=tk.W)
 
         sigmas_entry = tb.Entry(self.frame)
-        sigmas_entry.grid(column=1, row=3, padx=10, pady=10)
+        sigmas_entry.grid(column=1, row=4, padx=10, pady=10)
 
         # Generate Levels
         generate_button = tb.Button(self.frame, text="Generate", bootstyle="success")
@@ -75,20 +97,45 @@ class ContourWidget(BaseWidget):
         )
         self.close_button.grid(column=1, row=0, sticky=tk.SE, padx=(10, 0))
 
+    def dropdown_select(self, image):
+        self.selected_image = image
+        self.data_source_dropdown["text"] = (
+            image.file_name if image is not None else NOTHING_OPEN
+        )
 
-# Parameters label
-# 0, 0
+    def update_dropdown(self, selected_image, image_list):
+        # Always change the commands to match the current list
+        self.data_source_menu = tk.Menu(self.data_source_dropdown)  # TODO set tearoff
+        self.data_source_dropdown["menu"] = self.data_source_menu
 
-# Parameter X label in X+1, 1
-# Parameter X input in X+1, 2
+        selected_still_open = False
+        for image in image_list:
+            self.data_source_menu.add_command(
+                label=image.file_name,
+                command=partial(self.dropdown_select, image),
+            )
 
-# Sigma List label in 5, 0
-# Sigma List entry in 5, 1->end-1
+            if image == self.selected_image:
+                selected_still_open = True
 
-# Generate Levels button in 6, 0
+        if len(image_list) == 0:
+            # If the image list is empty, default text in the dropdown
+            self.dropdown_select(None)
+        elif not selected_still_open or self.get_data_source() is None:
+            # Otherwise if the selected image was closed
+            #   or we previously had nothing selected,
+            #   pick the new selected
+            self.dropdown_select(selected_image)
 
-# Levels label in 7, 0
-# Level list entry in 7, 1->end-1
+    def get_data_source(self):
+        return self.selected_image
 
-# Apply button in end, end
-# Close button in end, end
+
+# On widget open:
+# If there is at least one image open, put that in the dropdown as the selected image
+# If not, put "No image open"
+
+# When the image list changes:
+# Update the dropdown
+# BUT: If the image that is currently selected is closed, then fill out box with the currently selected image
+# BUT: If all images are closed, fill in "No image open"
