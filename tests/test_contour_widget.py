0e0efc51020fc4de2e89d2d5cc0a1f5b105f2747
Mitchell Merry <mitchellmerry145@gmail.com>
2023-10-08 23:29:19 +1100

    Add test for validation

---
+++ tests/test_contour_widget.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from src.widgets.contour_widget import validate_list_entry
+
+
+class ValidateTest(TestCase):
+    def test_validate_valid_cases(self):
+        cases = ["1", "23.4", "1,23.4", "12,3,4"]
+        for case in cases:
+            self.assertTrue(
+                validate_list_entry(case), f"Expected {case} to pass validation"
+            )
+
+    def test_validate_invalid_cases(self):
+        cases = ["", "234,", "a", "1a2"]
+        for case in cases:
+            self.assertFalse(
+                validate_list_entry(case), f"Expected {case} to fail validation"
+            )
