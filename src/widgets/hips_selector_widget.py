2c3adce1e3813234d642666e56312bbde1fa4b9c
RyanW <ryanwartmann@hotmail.com>
2023-10-14 00:47:46 +1100

    Changed hips selector to remain open after opening a hips survey Hips selector keeps a fixed size when switching between dropdown options Cleaned up hips selector code(Still more to do)

--- src/widgets/hips_selector_widget.py
+++ src/widgets/hips_selector_widget.py
@@ -34,6 +34,14 @@ xray_survey_options = ["ov-gso/P/RASS"]
 
 image_type_options = ["fits", "png", "jpg"]
 
+NO_IMAGE_SELECTED = "No Image Selected"
+
+NO_PROJECTION_SELECTED = "No Projection Selected"
+
+NO_SURVEY_SELECTED = "No Survey Selected"
+
+NO_IMAGE_TYPE_SELECTED = "No Image Type Selected"
+
 
 class HipsSelectorWidget(BaseWidget):
     label = "Hips Survey Selector"
@@ -41,6 +49,9 @@ class HipsSelectorWidget(BaseWidget):
 
     def __init__(self, root):
         super().__init__(root)
+        # self.geometry("750x325")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
         self.selected_projection = ""
         self.selected_hips_survey = ""
@@ -51,86 +62,122 @@ class HipsSelectorWidget(BaseWidget):
 
         self.setup()
 
-        self.grab_set()
-
     def setup(self):
-        frame = tb.Frame(self, width=100, bootstyle="light")
+        frame = tb.Frame(self, bootstyle="light")
         frame.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
 
         frame.columnconfigure((0, 1, 2, 3), weight=1)
         frame.rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
 
+        col_widths = [23, 37]
+
+        self.optical_dropdown = self.dropdown_options(
+            frame,
+            "Optical Hips Surveys",
+            NO_SURVEY_SELECTED,
+            optical_survey_options,
+            self.select_optical_survey,
+            col_widths[1],
+            0,
+            0,
+        )
+
+        self.infrared_dropdown = self.dropdown_options(
+            frame,
+            "Infrared Hips Surveys",
+            NO_SURVEY_SELECTED,
+            infrared_survey_options,
+            self.select_infrared_survey,
+            col_widths[1],
+            0,
+            1,
+        )
+
+        self.radio_dropdown = self.dropdown_options(
+            frame,
+            "Radio Hips Surveys",
+            NO_SURVEY_SELECTED,
+            radio_survey_options,
+            self.select_radio_survey,
+            col_widths[1],
+            0,
+            2,
+        )
+
+        self.xray_dropdown = self.dropdown_options(
+            frame,
+            "X-Ray Hips Surveys",
+            NO_SURVEY_SELECTED,
+            xray_survey_options,
+            self.select_xray_survey,
+            col_widths[1],
+            0,
+            3,
+        )
+
+        self.dropdown_options(
+            frame,
+            "Image Type",
+            NO_IMAGE_TYPE_SELECTED,
+            image_type_options,
+            self.select_image_type,
+            col_widths[1],
+            0,
+            5,
+        )
+
         self.ra_entry = tb.Entry(frame, bootstyle="dark")
         self.dec_entry = tb.Entry(frame, bootstyle="dark")
         self.FOV_entry = tb.Entry(frame, bootstyle="dark")
 
-        self.image_options(frame, "Open Images", 0, 0)
-
-        self.ra_option(frame, "Ra", 0, 1)
-
-        self.dec_option(frame, "Dec", 0, 2)
-
-        self.FOV_option(frame, "FOV", 0, 3)
-
-        self.projection_options(frame, "Projection", self.selected_projection, 0, 4)
-
-        self.optical_survey_options(
-            frame, "Optical Hips Surveys", self.selected_hips_survey, 2, 1
-        )
-
-        self.infrared_survey_options(
-            frame, "Infrared Hips Surveys", self.selected_hips_survey, 2, 2
-        )
-
-        self.radio_survey_options(
-            frame, "Radio Hips Surveys", self.selected_hips_survey, 2, 3
-        )
-
-        self.xray_survey_options(
-            frame, "X-Ray Hips Surveys", self.selected_hips_survey, 2, 4
-        )
-
-        self.image_type_options(frame, "Image Type", self.selected_image_type, 2, 0)
-
-        self.confirm_button(frame, "Select Survey", 2, 5)
-
-    def image_options(self, parent, text, gridX, gridY):
-        label = tb.Label(parent, text=text, bootstyle="inverse-light")
-        label.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
-        dropdown = tb.Menubutton(parent, text="No image selected", bootstyle="dark")
-        dropdown.grid(column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
-        dropdown_menu = tk.Menu(dropdown, tearoff=0)
-
-        dropdown_menu.add_command(
-            label="No image selected",
-            command=partial(self.select_image, None, dropdown),
-        )
-
+        # Might remove when sorting out right click option
         valid_images = []
         for image in self.root.image_controller.get_images():
-            if image.image_wcs is not None:
-                valid_images.append(image)
+            if image.image_wcs is None:
+                continue
 
-        for option in valid_images:
-            dropdown_menu.add_command(
-                label=option.file_name,
-                command=partial(self.select_image, option, dropdown),
-            )
+            valid_images.append(image)
 
-        dropdown["menu"] = dropdown_menu
+        self.dropdown_options(
+            frame,
+            "Open Images",
+            NO_IMAGE_SELECTED,
+            valid_images,
+            self.select_image,
+            col_widths[0],
+            2,
+            0,
+        )
 
-    def select_image(self, image, menu_button):
+        self.ra_option(frame, "Ra", 2, 1)
+
+        self.dec_option(frame, "Dec", 2, 2)
+
+        self.FOV_option(frame, "FOV", 2, 3)
+
+        self.dropdown_options(
+            frame,
+            "Projection",
+            NO_PROJECTION_SELECTED,
+            projection_options,
+            self.select_projection,
+            col_widths[0],
+            2,
+            4,
+        )
+
+        self.confirm_button(frame, "Select Survey", 3, 5)
+
+    def select_image(self, image, dropdown):
         if image is None:
             self.selected_wcs = None
-            menu_button["text"] = "No image selected"
+            dropdown["text"] = "No image selected"
             self.ra_entry.configure(state="enabled")
             self.dec_entry.configure(state="enabled")
             self.FOV_entry.configure(state="enabled")
         else:
             self.selected_wcs = image.image_wcs
-            menu_button["text"] = image.file_name
+            dropdown["text"] = image.file_name
             self.ra_entry.configure(state="disabled")
             self.dec_entry.configure(state="disabled")
             self.FOV_entry.configure(state="disabled")
@@ -159,163 +206,63 @@ class HipsSelectorWidget(BaseWidget):
             column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10
         )
 
-    def projection_options(self, parent, text, selected_projection, gridX, gridY):
+    def dropdown_options(
+        self, parent, text, dropdown_text, options, func, width, gridX, gridY
+    ):
         label = tb.Label(parent, text=text, bootstyle="inverse-light")
         label.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
 
-        dropdown = tb.Menubutton(parent, text=selected_projection, bootstyle="dark")
-        dropdown.grid(column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
+        dropdown = tb.Menubutton(
+            parent, text=dropdown_text, width=width, bootstyle="dark"
+        )
+        dropdown.grid(column=gridX + 1, row=gridY, sticky=tk.EW, padx=10, pady=10)
         dropdown_menu = tk.Menu(dropdown, tearoff=0)
 
-        for option in projection_options:
+        for option in options:
             dropdown_menu.add_command(
                 label=option,
-                command=partial(self.select_projection, option, dropdown),
+                command=partial(func, option, dropdown),
             )
 
         dropdown["menu"] = dropdown_menu
 
-    def select_projection(self, projection, menu_button):
+        return dropdown
+
+    def select_projection(self, projection, dropdown):
         self.selected_projection = projection
-        menu_button["text"] = projection
+        dropdown["text"] = projection
 
-    def optical_survey_options(self, parent, text, selected_survey, gridX, gridY):
-        label = tb.Label(parent, text=text, bootstyle="inverse-light")
-        label.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
-        self.optical_dropdown = tb.Menubutton(
-            parent, text=selected_survey, bootstyle="dark"
-        )
-        self.optical_dropdown.grid(
-            column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10
-        )
-
-        dropdown_menu = tk.Menu(self.optical_dropdown, tearoff=0)
-
-        for option in optical_survey_options:
-            dropdown_menu.add_command(
-                label=option,
-                command=partial(self.select_optical_survey, option),
-            )
-
-        self.optical_dropdown["menu"] = dropdown_menu
-
-    def select_optical_survey(self, hips_survey):
+    def select_optical_survey(self, hips_survey, dropdown):
         self.selected_hips_survey = hips_survey
-        self.optical_dropdown["text"] = hips_survey
-        self.infrared_dropdown["text"] = ""
-        self.radio_dropdown["text"] = ""
-        self.xray_dropdown["text"] = ""
+        dropdown["text"] = hips_survey
+        self.infrared_dropdown["text"] = NO_SURVEY_SELECTED
+        self.radio_dropdown["text"] = NO_SURVEY_SELECTED
+        self.xray_dropdown["text"] = NO_SURVEY_SELECTED
 
-    def infrared_survey_options(self, parent, text, selected_survey, gridX, gridY):
-        label = tb.Label(parent, text=text, bootstyle="inverse-light")
-        label.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
-        self.infrared_dropdown = tb.Menubutton(
-            parent, text=selected_survey, bootstyle="dark"
-        )
-        self.infrared_dropdown.grid(
-            column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10
-        )
-
-        dropdown_menu = tk.Menu(self.optical_dropdown, tearoff=0)
-
-        for option in infrared_survey_options:
-            dropdown_menu.add_command(
-                label=option,
-                command=partial(self.select_infrared_survey, option),
-            )
-
-        self.infrared_dropdown["menu"] = dropdown_menu
-
-    def select_infrared_survey(self, hips_survey):
+    def select_infrared_survey(self, hips_survey, dropdown):
         self.selected_hips_survey = hips_survey
-        self.infrared_dropdown["text"] = hips_survey
-        self.optical_dropdown["text"] = ""
-        self.radio_dropdown["text"] = ""
-        self.xray_dropdown["text"] = ""
+        dropdown["text"] = hips_survey
+        self.optical_dropdown["text"] = NO_SURVEY_SELECTED
+        self.radio_dropdown["text"] = NO_SURVEY_SELECTED
+        self.xray_dropdown["text"] = NO_SURVEY_SELECTED
 
-    def radio_survey_options(self, parent, text, selected_survey, gridX, gridY):
-        label = tb.Label(parent, text=text, bootstyle="inverse-light")
-        label.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
-        self.radio_dropdown = tb.Menubutton(
-            parent, text=selected_survey, bootstyle="dark"
-        )
-        self.radio_dropdown.grid(
-            column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10
-        )
-
-        dropdown_menu = tk.Menu(self.radio_dropdown, tearoff=0)
-
-        for option in radio_survey_options:
-            dropdown_menu.add_command(
-                label=option,
-                command=partial(self.select_radio_survey, option),
-            )
-
-        self.radio_dropdown["menu"] = dropdown_menu
-
-    def select_radio_survey(self, hips_survey):
+    def select_radio_survey(self, hips_survey, dropdown):
         self.selected_hips_survey = hips_survey
-        self.radio_dropdown["text"] = hips_survey
-        self.optical_dropdown["text"] = ""
-        self.infrared_dropdown["text"] = ""
-        self.xray_dropdown["text"] = ""
+        dropdown["text"] = hips_survey
+        self.optical_dropdown["text"] = NO_SURVEY_SELECTED
+        self.infrared_dropdown["text"] = NO_SURVEY_SELECTED
+        self.xray_dropdown["text"] = NO_SURVEY_SELECTED
 
-    def xray_survey_options(self, parent, text, selected_survey, gridX, gridY):
-        label = tb.Label(parent, text=text, bootstyle="inverse-light")
-        label.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
-        self.xray_dropdown = tb.Menubutton(
-            parent, text=selected_survey, bootstyle="dark"
-        )
-        self.xray_dropdown.grid(
-            column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10
-        )
-
-        dropdown_menu = tk.Menu(self.xray_dropdown, tearoff=0)
-
-        for option in xray_survey_options:
-            dropdown_menu.add_command(
-                label=option,
-                command=partial(self.select_xray_survey, option),
-            )
-
-        self.xray_dropdown["menu"] = dropdown_menu
-
-    def select_xray_survey(self, hips_survey):
+    def select_xray_survey(self, hips_survey, dropdown):
         self.selected_hips_survey = hips_survey
-        self.xray_dropdown["text"] = hips_survey
-        self.optical_dropdown["text"] = ""
-        self.infrared_dropdown["text"] = ""
-        self.radio_dropdown["text"] = ""
+        dropdown["text"] = hips_survey
+        self.optical_dropdown["text"] = NO_SURVEY_SELECTED
+        self.infrared_dropdown["text"] = NO_SURVEY_SELECTED
+        self.radio_dropdown["text"] = NO_SURVEY_SELECTED
 
-    def image_type_options(self, parent, text, selected_survey, gridX, gridY):
-        label = tb.Label(parent, text=text, bootstyle="inverse-light")
-        label.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
-
-        self.image_type_dropdown = tb.Menubutton(
-            parent, text=selected_survey, bootstyle="dark"
-        )
-        self.image_type_dropdown.grid(
-            column=gridX + 1, row=gridY, sticky=tk.NSEW, padx=10, pady=10
-        )
-
-        dropdown_menu = tk.Menu(self.image_type_dropdown, tearoff=0)
-
-        for option in image_type_options:
-            dropdown_menu.add_command(
-                label=option,
-                command=partial(self.select_image_type, option),
-            )
-
-        self.image_type_dropdown["menu"] = dropdown_menu
-
-    def select_image_type(self, type):
-        self.selected_image_type = type
-        self.image_type_dropdown["text"] = type
+    def select_image_type(self, image_type, dropdown):
+        self.selected_image_type = image_type
+        dropdown["text"] = image_type
 
     def confirm_button(self, parent, text, gridX, gridY):
         button = tb.Button(
@@ -324,7 +271,7 @@ class HipsSelectorWidget(BaseWidget):
             bootstyle="success",
             command=partial(self.select_survey),
         )
-        button.grid(column=gridX, row=gridY, sticky=tk.NSEW, padx=10, pady=10)
+        button.grid(column=gridX, row=gridY, sticky=tk.SE, padx=10, pady=10)
 
     def select_survey(self):
         if self.selected_wcs is None:
@@ -356,4 +303,4 @@ class HipsSelectorWidget(BaseWidget):
             self.hips_survey.survey = self.selected_hips_survey
             self.hips_survey.image_type = self.selected_image_type
 
-        self.close()
+        self.root.image_controller.open_hips(self.hips_survey, self.selected_wcs)
