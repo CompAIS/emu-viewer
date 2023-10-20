be0894ca28432f090c6c60e364b47ec63b98b6e9
Mitchell Merry <mitchellmerry145@gmail.com>
2023-10-20 19:07:36 +1100

    PX12-130 Change export to use get_tightbbox

--- src/lib/tool.py
+++ src/lib/tool.py
@@ -94,26 +94,26 @@ class NavigationToolbar(NavigationToolbar2Tk):
             fig = self.canvas.figure
             extent = (
                 fig.axes[0]
-                .get_window_extent()
+                .get_tightbbox(self.canvas.get_renderer())
                 .transformed(fig.dpi_scale_trans.inverted())
             )
-            print(extent)
+
             fig.savefig(fname, bbox_inches=extent)
         except Exception as e:
             tkinter.messagebox.showerror("Error saving file", str(e))
 
-    @staticmethod
-    def _mouse_event_to_message(event):
-        """
-        Stolen from base class. The base class was attaching some extra text to our tooltip.
-        We don't want that, so I've forcibly removed it here.
-        """
+    # @staticmethod
+    # def _mouse_event_to_message(event):
+    #     """
+    #     Stolen from base class. The base class was attaching some extra text to our tooltip.
+    #     We don't want that, so I've forcibly removed it here.
+    #     """
 
-        if event.inaxes and event.inaxes.get_navigate():
-            try:
-                s = event.inaxes.format_coord(event.xdata, event.ydata)
-            except (ValueError, OverflowError) as e:
-                print(e)
-            else:
-                return s.rstrip()
-        return ""
+    #     if event.inaxes and event.inaxes.get_navigate():
+    #         try:
+    #             s = event.inaxes.format_coord(event.xdata, event.ydata)
+    #         except (ValueError, OverflowError) as e:
+    #             print(e)
+    #         else:
+    #             return s.rstrip()
+    #     return ""
